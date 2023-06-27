ae1ac6b05545f81a6636a951e77713b99cb7252d
shravanbrick <shravan@getbrick.in>
2023-06-27 21:39:56 +0530

    constants fixed

--- src/constants/TBConstants.js
+++ src/constants/TBConstants.js
@@ -15,15 +15,5 @@ export const TBPriorityColor = {
 };
 
 export const TBLinks = {
-  helpAndSupport: `https://wa.me/message/AWX3WVA3UMTJO1`,
-  twitterURL: 'https://twitter.com/PillowFund',
-  mediumURL: 'https://pillowfund.medium.com/',
-  instagramURL: 'https://instagram.com/pillow.africa?igshid=YmMyMTA2M2Y=',
-  youtubeURL: 'https://www.youtube.com/@pillowfund/videos',
-  termsUrl:
-    'https://drive.google.com/file/d/1BHWvaiXxxHRd8sALuINzLAs1QsGbyaCN/view?usp=sharing',
-  policyUrl:
-    'https://drive.google.com/file/d/1BH2k9MyfjIHEPeW11AJVwtOAjpNvurHo/view?usp=sharing',
-  risksUrl:
-    'https://drive.google.com/file/d/1af7Xl2FEYBZboDE1BwGMwdZUtsDbu56R/view',
+  helpAndSupport: ``,
 };
